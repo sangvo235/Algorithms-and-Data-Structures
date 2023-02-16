c877e816b954c8284229d1887ccaa4a4ceddbb88
Sang Vo <sangvo235@gmail.com>
2023-02-16 23:18:34 +1100

    update

---
+++ Queues/Main.java
@@ -0,0 +1,31 @@
+package Queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Harold");
+
+        // System.out.println(queue.isEmpty());
+        // System.out.println(queue.size());
+        // System.out.println(queue.contains("Harold"));
+
+        // System.out.println(queue.peek());
+
+        // poll won't cause an expection
+        // queue.poll();
+        // queue.poll();
+        // queue.poll();
+        // queue.poll();
+        // queue.poll();
+
+        System.out.println(queue);
+    }
+}
---
+++ Stacks/Main.java
@@ -0,0 +1,30 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+
+        // Check if stack is empty
+        System.out.println(stack.empty());
+
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("Call of Duty");
+        stack.push("Boarderlands");
+        stack.push("World of Warcraft");
+
+        // stack.pop();
+        // stack.pop();
+
+        // System.out.println(stack.peek());
+
+        // System.out.println(stack.search("Skyrim"));
+
+        // System.out.println(stack);
+
+    }
+}
