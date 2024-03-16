773386d993569a15dc6e7bf9fccb89e204d24c40
sangvo235 <sangvo235@gmail.com>
2024-03-16 23:51:19 +0100

    code-update

--- Algorithms/Sort Algorithms/Bubble Sort/Main.java
+++ Algorithms/Sort Algorithms/Bubble Sort/Main.java
@@ -1,26 +1,38 @@
 public class Main {
-    public static void main(String[] args) {
 
-        int array[] = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
+    private static void bubbleSort(int arr[]) {
 
-        bubbleSort(array);
-
-        for (int i : array) {
-            System.out.println(i);
-        }
-    }
-
-    private static void bubbleSort(int array[]) {
-
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - i - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
 
     }
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 6, 2, 3, 9, 1, 8, 4, 7};
+
+        System.out.println("Unsorted array: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array: ");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+    }
+
 }
